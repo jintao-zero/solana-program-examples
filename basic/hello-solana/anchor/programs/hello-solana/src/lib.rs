1dba80b8bd691cb1646053b0cff50ab91c9072ca
jintao-zero <imjintao0128@gmail.com>
2025-03-23 11:22:43 +0800

    add hello solana example

--- basic/hello-solana/anchor/programs/hello-solana/src/lib.rs
+++ basic/hello-solana/anchor/programs/hello-solana/src/lib.rs
@@ -6,12 +6,13 @@ declare_id!("3LZsFm26YnY7WRS7gvCSSc2ZVbsuHwzakH5BdHrbVrHm");
 pub mod hello_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn hello(ctx: Context<Hello>) -> Result<()> {
         msg!("Hello, Solana!");
         msg!("Our program's Program ID is: {:?}", ctx.program_id);
+        msg!("Our program's Program ID is: {:?}", id());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Hello{}
