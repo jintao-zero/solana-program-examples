c4c7c3d4fa6fbbcd1373d566f547eb8163dafeb8
jintao-zero <imjintao0128@gmail.com>
2025-03-23 14:36:40 +0800

    add account data example

---
+++ basic/account-data/anchor/programs/account-data/src/instructions/create.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+use crate::state::AddressInfo;
+use crate::constants::*;
+
+#[derive(Accounts)]
+pub struct CreateAddressInfo<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + AddressInfo::INIT_SPACE, 
+    )]
+    pub address_info: Account<'info, AddressInfo>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn create_address_info(
+    ctx: Context<CreateAddressInfo>,
+        name: String,
+        house_number: u8,
+        street: String,
+        city: String,
+) -> Result<()> {
+    ctx.accounts.address_info.set_inner(AddressInfo{
+        name,
+        house_number,
+        street,
+        city
+    });
+    Ok(())
+}
---
+++ basic/account-data/anchor/programs/account-data/src/state/mod.rs
@@ -0,0 +1,4 @@
+
+pub mod address_info;
+
+pub use address_info::*;
